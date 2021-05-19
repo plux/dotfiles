04f9251c88b633d05f6762397cb8fab5653f2556
Hakan Nilsson <haakan@gmail.com>
2021-05-19 09:49:14 +0200

    update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,17 @@
+;; Adjust gc-cons-threshold. The default setting is too low.
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 100 1000 1000))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "%s [duration: %s] [gcs: %d]"
+                     (emacs-version)
+                     (format "%.2f s"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 ;; Stupid splash screen
 (setq inhibit-startup-message t)
 
@@ -49,6 +63,8 @@
     ([S-up]   . scroll-one-line-down)
     ("C-c a"  . align-regexp)
     ("C-c g"  . magit-status)
+    ("C-s"    . swiper)
+    ("C-c s"  . swiper-thing-at-point)
     ([f5]     . revert-buffer)
     ))
 
@@ -232,22 +248,14 @@
   )
 
 ;; LSP performance tweaks
-
-;; Adjust gc-cons-threshold. The default setting is too low for
-;; lsp-mode's needs due to the fact that client/server communication
-;; generates a lot of memory/garbage. You have two options:
-(setq gc-cons-threshold 100000000)
-
 ;; Increase the amount of data which Emacs reads from the
 ;; process. Again the emacs default is too low 4k considering that the
 ;; some of the language server responses are in 800k - 3M range.
-
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
 
 ;; Yasnippet
 (use-package yasnippet
   :ensure t
-  :defer t
   :hook (;(prog-mode . yas-minor-mode)
          (lsp-mode . yas-minor-mode))
   :config
@@ -335,10 +343,6 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(company-idle-delay 0.5)
-; '(company-quickhelp-color-background "#656565")
-; '(company-quickhelp-color-foreground "#ffffff")
-; '(company-quickhelp-delay 0.0)
-; '(company-quickhelp-mode t)
  '(custom-safe-themes
    '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "b0334e8e314ea69f745eabbb5c1817a173f5e9715493d63b592a8dc9c19a4de6" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" "78c4238956c3000f977300c8a079a3a8a8d4d9fee2e68bad91123b58a4aa8588" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" "6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" "7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "1c596673c1d111e95a404bd12f8dd446cbcd47eee885271e21ffc98c3ac386cb" "3e038e9133010baa92e17a2c57f87336e91e6e76139d8c38d7d55d3c59a15967" "682a1161ee456e2d715ba30be61697fdbce8c08e23c2c6a1943f155e3e52f701" "147a0b0fce798587628774ae804a18a73f121e7e5c5fdf3a874ba584fdbe131d" "4e96c6ca1ab443d9804bcb55104848b25bdfda5ae665adeb218db1af07e7979a" "e503f6b2f45ecc5c5e295d1b3d84bb484206c4badbf716847a2445facf9f7495" "fe2a620695413fe5dcd74e03f0383e577effd7bb59527aa4d86444108d861504" "2f57ee6507f30d3228cdddadd0150e7b2fd85dd7c818c2d6485888c7249c37e8" default))
  '(display-time-24hr-format t)
@@ -350,8 +354,9 @@
  '(flymake-fringe-indicator-position 'left-fringe)
  '(flymake-note-bitmap '(exclamation-mark compilation-info))
  '(package-selected-packages
-   '(company-fuzzy rust-mode diminish helm-xref eglot outline-toc company-box helm-swoop flycheck-pos-tip emojify flycheck-yang yang-mode dash soothe-theme spacemacs-theme color-theme-sanityinc-tomorrow flatland-theme gruvbox-theme counsel swiper-helm edts py-autopep8 blacken protobuf-mode company-jedi flycheck erlang slime projectile-ripgrep ripgrep iedit deft undo-tree know-your-http-well deadgrep helm-rg dumb-jump pdf-tools string-inflection use-package company-lsp lsp-mode ensime csv helm-projectile helm-ls-git helm-fuzzy-find ace-jump-buffer ace-jump-helm-line ac-helm helm-ag helm-git helm-themes helm-lobsters helm-pass apib-mode ht dash-functional org-journal yaml-mode nyan-mode multiple-cursors markdown-preview-mode magit haskell-mode go-mode forecast flymd flycheck-rust eproject elpy elm-mode editorconfig edit-server dockerfile-mode cider autotetris-mode ansible ag ace-jump-mode winner whitespace helm projectile lsp-ui which-key yasnippet company helm-lsp benchmark-init exec-path-from-shell))
+   '(dap-mode rainbow-delimiters company-fuzzy rust-mode diminish helm-xref eglot outline-toc company-box helm-swoop flycheck-pos-tip emojify flycheck-yang yang-mode dash soothe-theme spacemacs-theme color-theme-sanityinc-tomorrow flatland-theme gruvbox-theme counsel swiper-helm edts py-autopep8 blacken protobuf-mode company-jedi flycheck erlang slime projectile-ripgrep ripgrep iedit deft undo-tree know-your-http-well deadgrep helm-rg dumb-jump pdf-tools string-inflection use-package company-lsp lsp-mode ensime csv helm-projectile helm-ls-git helm-fuzzy-find ace-jump-buffer ace-jump-helm-line ac-helm helm-ag helm-git helm-themes helm-lobsters helm-pass apib-mode ht dash-functional org-journal yaml-mode nyan-mode multiple-cursors markdown-preview-mode magit haskell-mode go-mode forecast flymd flycheck-rust eproject elpy elm-mode editorconfig edit-server dockerfile-mode cider autotetris-mode ansible ag ace-jump-mode winner whitespace helm projectile lsp-ui which-key yasnippet company helm-lsp benchmark-init exec-path-from-shell))
  '(safe-local-variable-values '((vim . sw=2) (allout-layout . t)))
+ '(swiper-goto-start-of-match t)
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -383,6 +388,12 @@
  '(helm-match ((t (:foreground "#ffc125"))))
  '(helm-selection ((t (:background "gray25" :distant-foreground "black"))))
  '(helm-source-header ((t (:foreground "#cae682" :weight bold))))
- '(lsp-face-highlight-textual ((t (:weight bold)))))
+ '(ivy-current-match ((t (:extend t :background "#454545"))))
+ '(ivy-minibuffer-match-face-2 ((t (:underline t :weight ultra-bold))))
+ '(lsp-face-highlight-textual ((t (:weight bold))))
+ '(swiper-background-match-face-2 ((t (:inherit swiper-match-face-2))))
+ '(swiper-line-face ((t (:inherit nil :background "#454545"))))
+ '(swiper-match-face-2 ((t (:inverse-video t))))
+ '(whitespace-line ((t (:background "gray9")))))
 
 (put 'downcase-region 'disabled nil)
